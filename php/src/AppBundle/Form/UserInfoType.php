4213af8b0886a1c489d1bc9ea70de5bb82ec89ed
Sean Peterson <seanpeterson11@gmail.com>
2017-07-26 13:32:01 -0500

    add 2 working api calls that use updated form information.

--- php/src/AppBundle/Form/UserInfoType.php
+++ php/src/AppBundle/Form/UserInfoType.php
@@ -6,6 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Sonata\AdminBundle\Form\FormMapper;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\ResetTkype;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -15,12 +16,16 @@ class UserInfoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        ->add('income', TextType::class)
+        ->add('income', NumberType::class)
         ->add('city', ChoiceType::class, array(
             'choices'  => array(
-                'New York' => 'ny',
-                'Portland' => 'or',
-                'Los Angeles' => 'ca',
+              "New York" => 'New-York',
+              "Portland" => 'Portland',
+              "Los Angeles" => 'Los-Angeles',
+              "Denver" => 'Denver',
+              "Austin" => 'Austin',
+              "Nashville" => 'Nashville',
+              "Seattle" => 'Seattle',
             ),
         ))
         ->add('marital_status', ChoiceType::class, array(
