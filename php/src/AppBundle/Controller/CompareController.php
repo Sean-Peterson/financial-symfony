6e8968ba49c934cc82718e9fd8427fc668e92016
SeanPeterson <seanpeterson11@gmail.com>
2017-08-30 13:02:49 -0500

    add code for db branch when connected to mysql

--- php/src/AppBundle/Controller/CompareController.php
+++ php/src/AppBundle/Controller/CompareController.php
@@ -18,34 +18,6 @@ class CompareController extends Controller
      */
     public function compareAction(Request $request, EntityManagerInterface $em)
     {
-      // $db=[];
-      // $db['New-York']=['New-York','ny',1106,3006];
-      // $db['Portland']=['Portland','or',0.8118,1551];
-      // $db['Los-Angeles']=['Los-Angeles','ca',0.8228,1960];
-      // $db['Denver']=['Denver','co',0.8169,1577];
-      // $db['Austin']=['Austin','tx',0.7787,1574];
-      // $db['Nashville']=['Nashville','tn',0.7773,1478];
-      // $db['Seattle']=['Seattle','wa',0.9276,1875];
-      // $db['Washington']=['Washington','dc',0.943,2128];
-      // $db['San-Francisco']=['San-Francisco','ca',1.0146,3278];
-      // $db['Chicago']=['Chicago','il',0.8465,1802];
-      // $db['Boston']=['Boston','ma',0.9073,2442];
-      // $db['Minneapolis']=['Minneapolis','mn',0.8553,1349];
-      // $db['Dallas']=['Dallas','tx',0.6974,1229];
-      // $db['Houston']=['Houston','tx',0.7616,1356];
-      // $db['San-Jose']=['San-Jose','ca',0.8449,2414];
-      // $db['Indianapolis']=['Indianapolis','in',0.8078,1102];
-      // $db['Philadelphia']=['Philadelphia','pa',0.8743,1568];
-      // $db['San-Diego']=['San-Diego','ca',0.7908,1804];
-      // $db['Phoenix']=['Phoenix','az',0.7111,974];
-      // $db['Kansas-City']=['Kansas-City','mo',0.6883,909];
-      // $db['Miami']=['Miami','fl',0.9125,1879];
-      // $db['New-Orleans']=['New-Orleans','la',0.8199,1420];
-      // $db['Pittsburgh']=['Pittsburgh','pa',0.8173,1148];
-      // $db['Cleveland']=['Cleveland','oh',0.786,931];
-      // $db['Oklahoma-City']=['Oklahoma-City','ok',0.668,830];
-      // $db['Sacramento']=['Sacramento','ca',0.8236,1240];
-
       //create a repository full of the City objects
       $repo = $this->getDoctrine()->getRepository('AppBundle:City');
       //NY is bar for cost of living. So, all cities have a percentage point value that multiplies by NY col. Therefore I must always call NY and have the ability to read its values
